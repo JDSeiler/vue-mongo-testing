40addade1271d5b57ff38d3627d3eb6042ea448b
JDSeiler <jseiler@radford.edu>
2021-03-09 22:05:14 -0500

    Add Express server for doing Mongo things

---
+++ server/app-config.js
@@ -0,0 +1,6 @@
+const config = {
+    dbUri: 'mongodb://127.0.0.1:27017',
+    dbTable:'helo_mongo'
+}
+
+exports.config = config;
---
+++ server/index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const { config } = require('./app-config');
+const {DB_URI, MAIN_TABLE} = config;
+
+const mongo = require('mongodb');
+// Mongo yelled at me to turn on unified topology, so here we are
+const client = new mongo.MongoClient(DB_URI, { useUnifiedTopology: true });
+
+async function pushFact(fact) {
+    if (!client.isConnected()) {
+        await client.connect();
+    }
+
+    const facts = await client.db(MAIN_TABLE).collection("facts");
+    await facts.insertOne({"text": fact});
+}
+
+async function getFacts() {
+    if (!client.isConnected()) {
+        await client.connect();
+    }
+
+    const facts = await client.db(MAIN_TABLE).collection("facts");
+    const cursor = await facts.find({});
+    return await cursor.toArray();
+}
+
+async function deleteFacts() {
+    if (!client.isConnected()) {
+        await client.connect();
+    }
+
+    const facts = await client.db("hello_mongo").collection("facts");
+    await facts.deleteMany({});
+}
+
+// Bless the creator of this module
+// Appends: `Access-Control-Allow-Origin: *` to everything and makes my life easier.
+app.use(cors());
+
+/**
+ * POST /save
+ * Query parameters:
+ * fact: string
+ * 
+ * Saves the provided fact (passed as a query parameter) to MongoDB
+ */
+app.post('/save', (req, res) => {
+    const fact = req.query.fact;
+    if (typeof fact === 'undefined') {
+        res.sendStatus(400);
+    } else {
+        console.log(`Saving fact: ${fact}`);
+        res.send("Saving fact");
+        pushFact(fact);
+    }
+});
+
+/**
+ * GET /facts
+ * 
+ * Returns all facts from MongoDB
+ * 
+ * Response Shape:
+ * {
+ *  facts: [{id: string, text: string}] 
+ * }
+ */
+app.get('/facts', async function(req, res) {
+    const allFacts = await getFacts();
+    res.set('Content-Type', 'application/json');
+    res.send({facts: allFacts});
+});
+
+/**
+ * DELETE /facts
+ * 
+ * Deletes all facts from MongoDB
+ */
+app.delete('/facts', async function(req, res) {
+    await deleteFacts();
+    res.sendStatus(204);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000"));
